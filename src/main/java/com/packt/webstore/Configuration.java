923278ccba3916a7c117a21e54fa93202c46bfd3
Marta <bakowskamarta@gmail.com>
2017-12-30 22:44:04 +0100

    initial commit

---
+++ src/main/java/com/packt/webstore/Configuration.java
@@ -0,0 +1,17 @@
+package com.packt.webstore;
+
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.util.UrlPathHelper;
+
+@org.springframework.context.annotation.Configuration
+
+public class Configuration extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer){
+        UrlPathHelper urlPathHelper = new UrlPathHelper();
+        urlPathHelper.setRemoveSemicolonContent(false);
+        configurer.setUrlPathHelper(urlPathHelper);
+    }
+}
